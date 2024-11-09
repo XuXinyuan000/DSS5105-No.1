3c10d5541b9cb84aae770b2edd2844fd744e9d33
Chen Zhaoqi <E1351281@u.nus.edu>
2024-11-09 14:10:00 +0800

    Add files via upload

---
+++ Web/Web1/js/UploadButton.js
@@ -0,0 +1,19 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取上传按钮和隐藏的文件输入框
+    const uploadBtn = document.getElementById('uploadBtn');
+    const fileInput = document.getElementById('fileInput');
+
+    // 点击上传按钮时，触发文件选择框的点击事件
+    uploadBtn.addEventListener('click', function(event) {
+        event.preventDefault(); // 防止页面跳转
+        fileInput.click(); // 触发文件选择框
+    });
+
+    // 监听文件选择框的变化，获取选中的文件
+    fileInput.addEventListener('change', function() {
+        if (fileInput.files.length > 0) {
+            alert('你选择了文件: ' + fileInput.files[0].name);
+        }
+    });
+});
---
+++ Web/Web4/js/toggleContent.js
@@ -0,0 +1,9 @@
+function showMainContent() {
+    document.getElementById('mainContent').style.display = 'block';
+    document.getElementById('aboutUsContent').style.display = 'none';
+}
+
+function showAboutUsContent() {
+    document.getElementById('mainContent').style.display = 'none';
+    document.getElementById('aboutUsContent').style.display = 'flex';
+}
